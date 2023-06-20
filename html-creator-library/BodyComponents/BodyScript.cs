1061ebbbcbadc3f824ce871d4681db6985f32523
Vitaly05 <vitalik050405@gmail.com>
2023-06-20 03:37:19 +0300

    Added <script> in body and head

---
+++ html-creator-library/BodyComponents/BodyScript.cs
@@ -0,0 +1,18 @@
+﻿namespace html_creator_library.BodyComponents
+{
+    public class BodyScript : BodyComponent
+    {
+        private string scriptHtml;
+
+        public BodyScript(string src)
+            : base(new HtmlAttribute())
+        {
+            scriptHtml = $"<script src=\"{src}\"></script>";
+        }
+
+        internal override string GetHtml(string outTab)
+        {
+            return $"{outTab}{scriptHtml}";
+        }
+    }
+}
---
+++ html-creator-library/Script.cs
@@ -0,0 +1,17 @@
+﻿namespace html_creator_library.HeadComponents
+{
+    public class Script : HeadComponent
+    {
+        private string scriptHtml;
+
+        public Script(string src)
+        {
+            scriptHtml = $"<script src=\"{src}\"></script>";
+        }
+
+        internal override string GetHtml(string outTab)
+        {
+            return $"{outTab}{scriptHtml}";
+        }
+    }
+}
