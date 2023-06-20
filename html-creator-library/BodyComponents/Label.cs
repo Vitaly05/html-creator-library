99e276a19c32b02da542a04075bcdbdbc3f46193
Vitaly05 <vitalik050405@gmail.com>
2023-06-20 03:38:11 +0300

    Added <label>

---
+++ html-creator-library/BodyComponents/Label.cs
@@ -0,0 +1,22 @@
+﻿namespace html_creator_library.BodyComponents
+{
+    public class Label : BodyComponent
+    {
+        public Label(params BodyComponent[] components)
+            : this(new HtmlAttribute(), components) { }
+
+        public Label(HtmlAttribute attribute, params BodyComponent[] components)
+            : base(attribute)
+        {
+            innerHtmlComponents.AddRange(components);
+        }
+
+        internal override string GetHtml(string outTab)
+        {
+            innerTab = outTab + "\t";
+            return
+                $"{outTab}<label{attribute.GetAttributes()}>\n" +
+                $"{getInnerComponentsHtml()}\n" +
+                $"{outTab}</label>";        }
+    }
+}
