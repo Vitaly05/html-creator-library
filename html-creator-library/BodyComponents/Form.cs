cfef1535c22a77df8a81c571da539ded1b043d6b
Vitaly05 <vitalik050405@gmail.com>
2023-06-20 03:37:59 +0300

    Added <form>

---
+++ html-creator-library/BodyComponents/Form.cs
@@ -0,0 +1,23 @@
+﻿namespace html_creator_library.BodyComponents
+{
+    public class Form : BodyComponent
+    {
+        public Form(params BodyComponent[] components)
+            : this(new HtmlAttribute(), components) { }
+
+        public Form(HtmlAttribute attribute, params BodyComponent[] components)
+            : base(attribute)
+        {
+            innerHtmlComponents.AddRange(components);
+        }
+
+        internal override string GetHtml(string outTab)
+        {
+            innerTab = outTab + "\t";
+            return
+                $"{outTab}<form{attribute.GetAttributes()}>\n" +
+                $"{getInnerComponentsHtml()}\n" +
+                $"{outTab}</form>";
+        }
+    }
+}
