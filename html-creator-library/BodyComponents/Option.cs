db70e629ecab7bd1323cc2ad4a1e301ee8fafc29
Vitaly05 <vitalik050405@gmail.com>
2023-06-20 03:37:41 +0300

    Added <select> with <option>

---
+++ html-creator-library/BodyComponents/Option.cs
@@ -0,0 +1,28 @@
+﻿namespace html_creator_library.BodyComponents
+{
+    public class Option : BodyComponent
+    {
+        private string value;
+        private bool selected;
+
+        public Option(string value, bool selected, params BodyComponent[] components)
+            : this (value, selected, new HtmlAttribute(), components) { }
+
+        public Option(string value, bool selected, HtmlAttribute attribute, params BodyComponent[] components)
+            : base(attribute)
+        {
+            this.value = value;
+            this.selected = selected;
+            innerHtmlComponents.AddRange(components);
+        }
+
+        internal override string GetHtml(string outTab)
+        {
+            innerTab = outTab + "\t";
+            return
+                $"{outTab}<option value=\"{value}\" {(selected ? "selected" : "")}{attribute.GetAttributes()}>\n" +
+                $"{getInnerComponentsHtml()}\n" +
+                $"{outTab}</option>";
+        }
+    }
+}
---
+++ html-creator-library/BodyComponents/Select.cs
@@ -0,0 +1,23 @@
+﻿namespace html_creator_library.BodyComponents
+{
+    public class Select : BodyComponent
+    {
+        public Select(params BodyComponent[] components)
+            : this(new HtmlAttribute(), components) { }
+
+        public Select(HtmlAttribute attribute, params BodyComponent[] components)
+            : base(attribute)
+        {
+            innerHtmlComponents.AddRange(components);
+        }
+
+        internal override string GetHtml(string outTab)
+        {
+            innerTab = outTab + "\t";
+            return
+                $"{outTab}<select{attribute.GetAttributes()}>\n" +
+                $"{getInnerComponentsHtml()}\n" +
+                $"{outTab}</select>";
+        }
+    }
+}
